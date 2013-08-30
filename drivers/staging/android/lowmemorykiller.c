f14baaf89215fe6a35c8a7ef1f0829e022feb683
Liam Mark <lmark@codeaurora.org>
2013-08-30 12:10:39 -0700

    android/lowmemorykiller: Account for total_swapcache_pages
    The lowmemorykiller relies on NR_FILE_PAGES when measuring
    the amount of reclaimable memory in the system.
    However when swap is enabled swap cache pages are counted
    in NR_FILE_PAGES, and swap cache pages aren't as reclaimable
    in low memory as file cache pages. Therefore a large swap
    cache can result in the lowmemorykiller not running and
    an OOM occurring.
    
    In order to ensure the lowmemorykiller properly evaluates the
    amount of reclaimable memory don't count the swap cache pages.
    
    Change-Id: I38239283e572f814b277c718eaf6be7f92abacbb
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -46,12 +46,6 @@
 #include <linux/delay.h>
 #include <linux/swap.h>
 #include <linux/fs.h>
-#include <linux/cpuset.h>
-#include <linux/vmpressure.h>
-#include <linux/zcache.h>
-
-#define CREATE_TRACE_POINTS
-#include <trace/events/almk.h>
 
 #ifdef CONFIG_HIGHMEM
 #define _ZONE ZONE_HIGHMEM
@@ -598,10 +592,16 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		return 0;
 
 	other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
-	other_file = global_node_page_state(NR_FILE_PAGES) -
+
+	if (global_node_page_state(NR_SHMEM) + total_swapcache_pages() +
+			global_node_page_state(NR_UNEVICTABLE) <
+			global_node_page_state(NR_FILE_PAGES))
+		other_file = global_node_page_state(NR_FILE_PAGES) -
 					global_node_page_state(NR_SHMEM) -
 					global_node_page_state(NR_UNEVICTABLE) -
 					total_swapcache_pages();
+	else
+		other_file = 0;
 
 	tune_lmk_param(&other_free, &other_file, sc);
 
