0c81f0b8d284a80398a2a241d7fc4de5486c6d08
Joel Fernandes <joelaf@google.com>
2017-07-21 09:41:06 -0700

    ANDROID: lowmemorykiller: Add tgid to kill message
    Bug: 33346201
    Test: trigger LMK and verify tgid is there
    
    Change-Id: I047abc3aa541522766d2a84ebb4c77caf57a18a3
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -833,36 +833,17 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			task_set_lmk_waiting(selected);
 		task_unlock(selected);
 		trace_lowmemory_kill(selected, cache_size, cache_limit, free);
-		lowmem_print(1, "Killing '%s' (%d), adj %hd,\n"
-			"to free %ldkB on behalf of '%s' (%d) because\n"
-			"cache %ldkB is below limit %ldkB for oom score %hd\n"
-			"Free memory is %ldkB above reserved.\n"
-			"Free CMA is %ldkB\n"
-			"Total reserve is %ldkB\n"
-			"Total free pages is %ldkB\n"
-			"Total file cache is %ldkB\n"
-			"GFP mask is 0x%x\n",
-			selected->comm, selected->pid,
-			selected_oom_score_adj,
-			selected_tasksize * (long)(PAGE_SIZE / 1024),
-			current->comm, current->pid,
-			cache_size, cache_limit,
-			min_score_adj,
-			free,
-			global_page_state(NR_FREE_CMA_PAGES) *
-			(long)(PAGE_SIZE / 1024),
-			totalreserve_pages * (long)(PAGE_SIZE / 1024),
-			global_page_state(NR_FREE_PAGES) *
-			(long)(PAGE_SIZE / 1024),
-			global_node_page_state(NR_FILE_PAGES) *
-			(long)(PAGE_SIZE / 1024),
-			sc->gfp_mask);
-
-		if (lowmem_debug_level >= 2 && selected_oom_score_adj == 0) {
-			show_mem(SHOW_MEM_FILTER_NODES);
-			dump_tasks(NULL, NULL);
-		}
-
+		lowmem_print(1, "Killing '%s' (%d) (tgid %d), adj %hd,\n"
+				 "   to free %ldkB on behalf of '%s' (%d) because\n"
+				 "   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n"
+				 "   Free memory is %ldkB above reserved\n",
+			     selected->comm, selected->pid, selected->tgid,
+			     selected_oom_score_adj,
+			     selected_tasksize * (long)(PAGE_SIZE / 1024),
+			     current->comm, current->pid,
+			     cache_size, cache_limit,
+			     min_score_adj,
+			     free);
 		lowmem_deathpending_timeout = jiffies + HZ;
 		rem += selected_tasksize;
 		rcu_read_unlock();
